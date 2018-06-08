08499fcf6f0d10bae79789f9bb3ae0dd35634811
Rhalp Darren Cabrera <rhalpdarrencabrera@gmail.com>
2018-06-08 20:06:11 +0800

    Delete footer.php

--- footer.php
+++
@@ -1,5 +0,0 @@
-					<!-- Footer -->
-					<div class="footer navbar-fixed-bottom text-muted ">
-						&copy; 2015. <a href="#">Limitless Web App Kit</a> by <a href="http://themeforest.net/user/Kopyov" target="_blank">zxcasd</a>
-					</div>
-					<!-- /footer -->
